41dca7fad302f89321bc2559fc9773bec5d92fdf
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2023-07-08 12:56:36 +0530

    Completed the search Exercises field creation and button creation

---
+++ src/utils/fetchData.js
@@ -0,0 +1,9 @@
+// Connection with rapid Api
+
+
+export const fetchData = async(url,options)=>{
+    const response = await fetch(url, options);
+    const data = await response.json()
+
+    return data;
+}
