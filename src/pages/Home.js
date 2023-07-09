543c6233dbddf6f7fcdcd27177f2123d4d39c071
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2023-07-09 19:44:05 +0530

    Home Page is Finalized

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,7 @@ import SeachExercises from '../components/SeachExercises'
 const Home = () => {
   const [exercises,setExercises]  = useState([]);
   const [bodyPart,setBodyPart]  = useState('all');
+  console.log(bodyPart)
   return (
     <Box>
       <HeroBanner />
