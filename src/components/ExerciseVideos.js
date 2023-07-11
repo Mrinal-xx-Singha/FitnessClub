c3a0da48e22eaad6b912aab1d211273815b9d95d
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2023-07-11 14:25:51 +0530

    Completed the footer and the website

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -14,7 +14,7 @@ const ExerciseVideos = ({exerciseVideos, name}) => {
       </Typography>
       <Stack sx={{flexDirection : {lg:'row'}, gap:{ lg:'110px',xs:'0'}
       }} justifyContent='flex-start' flexWrap='wrap' alignItems='center'>
-        { exerciseVideos?.slice(0,3).map((item,index) => (
+        { exerciseVideos?.slice(0,6).map((item,index) => (
 
           <a
             key={index} className='exercise-video'
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import {Box, Stack , Typography} from '@mui/material';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px' sx={{backgroundColor:'#777799'}}>
+        <Typography variant='h5' pd='40px' mt='20px' mb='20px'>
+          Made With 🤍 By Mrinal Singha
+          <br />ig_mrinalsingha
+        </Typography>
+      </Stack>
+    </Box>
+  
   )
 }
 
